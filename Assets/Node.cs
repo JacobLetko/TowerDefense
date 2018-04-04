8389b0617b26340bb65f2956b4af21e9224e361f
Jacob Letko <s179061@students.aie.edu.au>
2018-04-04 11:08:47 -0700

    it all works

---
+++ Assets/Node.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Color startColor;
+
+    private Renderer rend;
+
+    public GameObject turretToBuild;
+    private GameObject Turret;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if(Turret != null)
+        {
+            Debug.Log("Cant Build There");
+            return;
+        }
+        else
+        {
+            Turret = Instantiate(turretToBuild);
+            Turret.transform.position = transform.position;
+        }
+
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
